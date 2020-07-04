d0a957ab8ceb05b3b91ea3a6a317989826fff3fc
dileepkumarhb <dileepbannikatti@gmail.com>
2020-07-04 21:47:14 +0530

    new change

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CartItem from "./CartItem";
+//import CartItem from "./CartItem";
 import {Link} from 'react-router-dom';
 export default function CartTotals({value}) {
 	const{cartSubTotal,cartTax,cartTotal,clearCart}=value;
