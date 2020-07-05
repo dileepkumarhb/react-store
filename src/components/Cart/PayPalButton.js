23cf7d31e0441066b015368dc34e68f7b2694d66
dileepkumarhb <dileepbannikatti@gmail.com>
2020-07-05 14:54:51 +0530

    sandbox err

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -30,7 +30,7 @@ export default class MyApp extends React.Component {
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
         const client = {
-            sandbox:process.env.REACT_APP_APP_ID,
+            sandbox:AdlNb4DTLx7t9L33z9w0rSypyzYNavvONOD-GMC3lLjVO6c-rTZi4Gpq2mxx8epTCsAR1dX4j_maGJUu,
             production: 'YOUR-PRODUCTION-APP-ID',
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
